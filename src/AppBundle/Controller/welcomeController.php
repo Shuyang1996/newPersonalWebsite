03740e2c17948d4ec5cc13a3bb53478874d3c38a
Shuyang Yu <williamysy@gmail.com>
2016-01-28 00:07:34 -0500

    editing front page

---
+++ src/AppBundle/Controller/welcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class welcomeController extends Controller{
+	/**
+	*@Route("/")
+	*/
+	public function frontpageAction(){
+		return new response('<html><p>Welcome to the first page</p></html>');
+	}
+}
